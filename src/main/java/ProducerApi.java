ecee34b4cb0d65ecef1b85f9d3d36ba7603b677d
YOUNGLOT <gnszja20@redwoodk.com>
2021-03-26 13:37:44 +0900

    주석 & 코드 정리

--- src/main/java/ProducerApi.java
+++ src/main/java/ProducerApi.java
@@ -1,4 +1,3 @@
-
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
@@ -21,69 +20,91 @@ public class ProducerApi {
     private static final int REMOTE_PORT = 10022;
     private static final int SESSION_TIMEOUT = 100000;
     private static final int CHANNEL_TIMEOUT = 100000;
-    private final String TOPIC_NAME = "iu01";
-    //private final String FIN_MESSAGE = "exit";
+
+    private static final String KAFKA_SERVER = "yh01:9092";
+    private static final String TOPIC_NAME = "iu01";
+
+    private static final String COMMAND = "tail -f /work/logs/modules/web/hydrak.log";
 
     public static void main(String[] args) {
-        String command = "tail -f /work/logs/modules/web/hydrak.log";
+
         ProducerApi producerApi = new ProducerApi();
-        producerApi.executeCommand(command);
+        producerApi.executeCommand(COMMAND);
 
     }
 
     private void executeCommand(String executeCommand) {
         //  lib 이름이 jsch 여서 명명함
+        JSch jsch = null;
+        //  sftp 연결 Session
         Session jschSession = null;
+        //  jsch lib에서 실행 객체
+        ChannelExec channelExec = null;
+        //  sftp 에서 결과 를 받아 올 Stream
+        InputStream inputStream = null;
+
+        //  카프카 Properties
+        Properties kafkaProperties = null; // kafka properties
+        //  카프카 Producer
+        KafkaProducer<String, String> producer = null;
 
         try {
+            //  카프카 Producer
+            kafkaProperties = new Properties();
 
-            Properties properties = new Properties();
+            kafkaProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+            kafkaProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            kafkaProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-            properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "yh01:9092");
-            properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-            properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            producer = new KafkaProducer<>(kafkaProperties);
 
-            KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
-
-
-            JSch jsch = new JSch();
+            //  sftp 연결
+            jsch = new JSch();
             jschSession = jsch.getSession(USERNAME, REMOTE_HOST, REMOTE_PORT);
             jschSession.setConfig("StrictHostKeyChecking", "no");
             jschSession.setPassword(PASSWORD);
             jschSession.connect(SESSION_TIMEOUT);
 
-            ChannelExec channelExec = (ChannelExec) jschSession.openChannel("exec");
-
+            //  Command 실행
+            channelExec = (ChannelExec) jschSession.openChannel("exec");
             channelExec.setCommand(executeCommand);
-            channelExec.setErrStream(System.err);
-            InputStream inputStream = channelExec.getInputStream();
-            channelExec.connect(CHANNEL_TIMEOUT);
-            byte[] tmp = new byte[1024];
 
+            //  결과 가져오기 Using Stream
+            channelExec.setErrStream(System.err);
+            inputStream = channelExec.getInputStream();
+            channelExec.connect(CHANNEL_TIMEOUT);
+
+            byte[] tmp = new byte[1024];
             while (true) {
 
                 while (inputStream.available() > 0) {
                     int i = inputStream.read(tmp, 0, 1024);
                     if (i < 0) break;
 
-                    System.out.println(new String(tmp, 0, i));
+                    //region Producer send
 
-                    ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, new String(tmp, 0, i));
+                    String result = new String(tmp, 0, i);
+
+                    System.out.println(result);
+                    ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, result);
 
                     try {
                         producer.send(record, (metadata, exception) -> {
                             if (exception != null) {
                                 // some exception
+                                System.out.println("------------------------------------ exception ------------------------------------");
                             }
                         });
 
                     } catch (Exception e) {
+                        System.out.println("Kafka Producer Send Exception");
+                        e.printStackTrace();
                         // exception
                     } finally {
                         producer.flush();
                     }
-                    //System.out.println(new String(tmp, 0, i));
 
+                    //endregion
                 }
 
                 if (channelExec.isClosed()) {
@@ -92,7 +113,7 @@ public class ProducerApi {
                 }
 
                 try {
-                    Thread.sleep(50);
+                    Thread.sleep(500);
                 } catch (Exception e) {
                 }
             }
